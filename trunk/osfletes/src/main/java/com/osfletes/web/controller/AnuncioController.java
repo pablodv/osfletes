d4b6e45e34fcecd0050dacce91b1eea894f61b53
gaston.dubra@gmail.com <gaston.dubra@gmail.com@a12c1719-fec6-b897-e568-3e1b6a76a5d8>
2011-11-08 22:33:30 +0000

    bla

--- trunk/osfletes/src/main/java/com/osfletes/web/controller/AnuncioController.java
+++ trunk/osfletes/src/main/java/com/osfletes/web/controller/AnuncioController.java
@@ -3,15 +3,25 @@ package com.osfletes.web.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.osfletes.service.IAnuncioService;
 
 @Controller
 public class AnuncioController {
 	
-	@RequestMapping(value="/hola")
+	IAnuncioService anuncioService;
+	
+	@Autowired
+	public void setAnuncioService(IAnuncioService anuncioService) {
+		this.anuncioService = anuncioService;
+	}
+
+
+	@RequestMapping(value="/anuncio")
 	public String saveAnuncio(HttpServletRequest request, HttpServletResponse response){
-		
 		return "redirect:index"; 
 	}
 	

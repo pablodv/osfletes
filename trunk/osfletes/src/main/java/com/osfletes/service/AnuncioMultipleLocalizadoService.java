ced62f8161b7d4efdd5eab24e08386dc066f5126
matiaskochman@gmail.com <matiaskochman@gmail.com@a12c1719-fec6-b897-e568-3e1b6a76a5d8>
2011-12-20 17:16:29 +0000

    creando test para anuncio

--- trunk/osfletes/src/main/java/com/osfletes/service/AnuncioMultipleLocalizadoService.java
+++ trunk/osfletes/src/main/java/com/osfletes/service/AnuncioMultipleLocalizadoService.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.osfletes.dao.hibernate.AnuncioMultipleLocalizadoDAO;
 import com.osfletes.model.AnuncioMultipleLocalizado;
-import com.osfletes.model.Direccion;
 import com.osfletes.service.interfaces.IAnuncioService;
 import com.osfletes.service.interfaces.IDireccionService;
 import com.osfletes.web.dto.FiltroDTO;
@@ -24,25 +22,11 @@ public class AnuncioMultipleLocalizadoService extends GenericServiceImplementaci
 	public void setDireccionService(IDireccionService direccionService) {
 		this.direccionService = direccionService;
 	}
-
-	
-
-	@Transactional
-	public void saveWithAddresses(AnuncioMultipleLocalizado anuncio,List<Direccion> addresses) {
-		for (Direccion direccion : addresses) {
-			//direccion.owner = anuncio;
-		}
-		this.dao.save(anuncio);
-	}	
-	
 	
 
 	@Override
 	public List<AnuncioMultipleLocalizado> findAnuncios(FiltroDTO filtro) {
 		return this.dao.findAnuncios(filtro);
 	}
-
-	
-	
 
 }

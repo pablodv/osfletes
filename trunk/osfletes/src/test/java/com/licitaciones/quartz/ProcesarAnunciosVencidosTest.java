efd79817600b06894dc5fa53b6f4608abb90dad5
matiaskochman@gmail.com <matiaskochman@gmail.com@a12c1719-fec6-b897-e568-3e1b6a76a5d8>
2011-12-26 20:02:02 +0000

    quartz

--- trunk/osfletes/src/test/java/com/licitaciones/quartz/ProcesarAnunciosVencidosTest.java
+++ trunk/osfletes/src/test/java/com/licitaciones/quartz/ProcesarAnunciosVencidosTest.java
@@ -8,24 +8,24 @@ import java.text.ParseException;
 import java.util.Date;
 
 import org.junit.Test;
-import org.quartz.CronTrigger;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
+
+import ch.qos.logback.classic.Logger;
 
 import com.osfletes.quartz.ProcesarAnunciosVencidos;
 public class ProcesarAnunciosVencidosTest {
 
-    Logger log = LoggerFactory.getLogger(ProcesarAnunciosVencidosTest.class);
 	
 	@Test
 	public void procesarAnunciosVencidosTest(){
         // Grab the Scheduler instance from the Factory 
         Scheduler scheduler;
+        //Logger log = (Logger) LoggerFactory.getLogger(ProcesarAnunciosVencidosTest.class);
 		try {
 			scheduler = StdSchedulerFactory.getDefaultScheduler();
 			// and start it off
@@ -43,17 +43,18 @@ public class ProcesarAnunciosVencidosTest {
 				    .withIdentity("trigger1", "group1")
 				    .withSchedule(cronSchedule("0/20 * * * * ?"))
 				    .build();
-				scheduler.scheduleJob(job, trigger);			
+				//scheduler.scheduleJob(job, trigger);			
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
 	        
 			
 	        Date ft = scheduler.scheduleJob(job, trigger);
+	        /*
 	        log.info(job.getKey() + " has been scheduled to run at: " + ft
 	                + " and repeat based on expression: "
 	                + ((CronTrigger) trigger).getCronExpression());
-			
+			*/
 	        // Tell quartz to schedule the job using our trigger
 			scheduler.start();
 			
